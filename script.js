fc2355517a5c3450be718da842b8c8ce3124574b
Mahir Durna <durna.m@outlook.de>
2024-05-18 20:15:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,80 @@
+let names = ["Mahir", "Svetlana"];
+let yournotes = ["Test", "Test2"];
+load();
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll("[data-include]");
+    for (let i = 0; i < includeElements.length; i++) {
+        let element = includeElements[i];
+        let file = element.getAttribute("data-include");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = "Page not found";
+        }
+    }
+}
+
+function render() {
+    let noteContainer = document.getElementById("content");
+    noteContainer.innerHTML = '';
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const yournote = yournotes[i];
+
+        noteContainer.innerHTML += /*html*/ `
+                <div id="notiz" class="cardNote">
+                    <p>Name: </p> ${name} <br>
+                    <p>Ihre Notiz: </p> ${yournote}
+                    <button onclick="deleteNots(${i})">Löschen</button>
+                </div>
+            `;
+    }
+}
+
+function addYourNotes() {
+    let name = document.getElementById("name");
+    let note = document.getElementById("note");
+
+    names.push(name.value);
+    yournotes.push(note.value);
+
+    name.value = "";
+    note.value = "";
+
+    render();
+    save();
+}
+
+function deleteNots(i) {
+    names.splice(i, 1);
+    yournotes.splice(i, 1);
+
+    render();
+    save();
+}
+
+function save() {
+    let namesAsText = JSON.stringify(names);
+    localStorage.setItem("names", namesAsText);
+
+    let yournotesAsText = JSON.stringify(yournotes);
+    localStorage.setItem("yournotes", yournotesAsText);
+}
+
+function load() {
+    let namesAsText = localStorage.getItem("names");
+    let yournotesAsText = localStorage.getItem("yournotes");
+
+    if (namesAsText && yournotesAsText) {
+        names = JSON.parse(namesAsText);
+        yournotes = JSON.parse(yournotesAsText);
+    }
+}
+
+function autoResizeTextarea(textarea) {
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
+}
